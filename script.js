acccf0bb124e4f27bd6a1ddaee996eeeed845cfe
kwaspek104 <71158863+kwaspek104@users.noreply.github.com>
2020-10-25 02:03:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,18 +10,19 @@ function writePassword() {
 
 var upperCase  = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePasswordOptions() {
+function generatePassword() {
 
 var lowerCaseInput;
 var upperCaseInput;
 var numericInput;
 var specialCharactersInput;
+var options;
 
 var length = prompt("Hi cutie. ;) Please choose a number from 8-128. This will be how many characters your password will have.")
 if (length < 8 || length > 128) {
@@ -64,46 +65,75 @@ if (!lowerCaseInput  && !upperCaseInput  && !numericInput  && !specialCharacters
 return alert("Oops! You need to have selected at least one criteria. Please try again!")
 }
 
-var passwordOptions = {
-length: length, 
-containsLowerCase: lowerCaseInput,  
-cotainsUpperCase: upperCaseInput,
-conatinsNumeric: numericInput,
-containsSpecialCharacters: specialCharactersInput,
-} 
-
-return passwordOptions
+else if (lowerCaseInput) {
+options = lowerCase;
 }
 
-function generatePassword () {
+else if (upperCaseInput) {
+options = upperCase;
+}
 
-var passwordPool = []
+else if (numericInput) {
+options = numeric;
+}
+
+else if (specialCharactersInput) {
+options = specialCharacters;
+}
+
+else if (lowerCaseInput && upperCaseInput) {
+options = lowerCase.concat(upperCase);
+}
+
+else if (lowerCaseInput && numericInput) {
+options = lowerCase.concat(numeric);  
+}
+
+else if (lowerCaseInput && specialCharactersInput) {
+options = lowerCase.concat(specialCharacters);  
+}
+
+else if (upperCaseInput && numericInput) {
+options = upperCase.concat(numeric);
+}
+
+else if (upperCaseInput && specialCharactersInput) {
+options = upperCase.concat(specialCharacters);
+}
+
+else if (numericInput && specialCharactersInput) {
+options = numeric.concat(specialCharacters);
+}
+
+else if (lowerCaseInput && upperCaseInput && numericInput) {
+options = lowerCase.concat(upperCase, numeric);
+}
+
+else if (lowerCaseInput && upperCaseInput && specialCharactersInput) {
+options = lowerCase.concat(upperCase, specialCharacters);
+}
+
+else if (lowerCaseInput && numericInput && specialCharactersInput) {
+options = lowerCase.concat(numeric, specialCharacters);
+}
+
+else if (upperCaseInput && numericInput && specialCharactersInput) {
+options = upperCase.concat(numeric, specialCharacters);
+}
+
+else if (lowerCaseInput && upperCaseInput && numericInput && specialCharactersInput) {
+options = lowerCase.concat(upperCase, numeric, specialCharacters);
+}
+
 var results = []
-var options = generatePasswordOptions()
 
-if (options.containsLowerCase) {
-passwordPool = passwordPool.concat(lowerCase)  
-}
-
-if (options.containsUpperCase) {
-passwordPool = passwordPool.concat(upperCase)  
-}
-
-if (options.conatinsNumeric) {
-passwordPool = passwordPool.concat(numeric)  
-}
-
-if (options.containsSpecialCharacters) {
-passwordPool = passwordPool.concat(specialCharacters)  
-}
-
-for (var i = 0; i < options.length; i++) {
-var temp = passwordPool[Math.floor(Math.random() * options.length)];
+for (var i = 0; i < length; i++) {
+var temp = options[Math.floor(Math.random() * options.length)];
 results.push(temp);
-  
 };
-console.log (`PasswordPool: ${passwordPool}`)
-console.log(`Results: ${results}`)
+
+console.log(options)
 
 return results.join("")
+
 }
